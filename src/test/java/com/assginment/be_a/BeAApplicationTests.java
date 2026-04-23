1eb71a0c3b01728ca5b22d381946d9d02177527d
dltjdska32 <dltjdska32@naver.com>
2026-04-23 14:02:55 +0900

    INIT

---
+++ src/test/java/com/assginment/be_a/BeAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assginment.be_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
